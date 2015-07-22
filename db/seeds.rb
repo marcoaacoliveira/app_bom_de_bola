40cfc2a98bfe23559cff2fe01fb3aeeccab765ba
marcoaacoliveira <marcoaacoliveira@gmail.com>
2015-07-22 13:25:37 -0300

    Adicionado arquivo  csv com informações sobre os times.  Criado no seed uma importação pra o csv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-u = User.all
-u.each  do |x|
-  x.destroy
-end
+
 User.create(email:'marcoaacoliveira@gmail.com', password:'123456', password_confirmation: '123456', name:'Marco Oliveira', admin: true)
 User.create(email:'coach@gmail.com', password:'123456', password_confirmation: '123456', name:'Coach', admin: false)
+
+# importando .csv
+require 'csv'
+
+CSV.foreach("db/csv/equipes_bom_de_bola.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Team.create(row.to_hash)
+end
