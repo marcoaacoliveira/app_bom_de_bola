146aa399cba21e759bc346341b6be305a1b7f50b
marcoaacoliveira <marcoaacoliveira@gmail.com>
2015-07-22 12:46:40 -0300

    Alterando as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email:'marcoaacoliveira@gmail.com', password:'123456', password_confirmation: '123456', name:'Marco Oliveira')
+u = User.all
+u.each  do |x|
+  x.destroy
+end
+User.create(email:'marcoaacoliveira@gmail.com', password:'123456', password_confirmation: '123456', name:'Marco Oliveira', admin: true)
+User.create(email:'coach@gmail.com', password:'123456', password_confirmation: '123456', name:'Coach', admin: false)
