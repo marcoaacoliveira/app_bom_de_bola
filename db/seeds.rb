2d435c6915072a3a7d9cdb4e2d50d10aa77b9bf9
marcoaacoliveira <marcoaacoliveira@gmail.com>
2015-07-23 00:22:22 -0300

    Alterado seeds e ajustado index para  jogadores

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+u = User.all
+u.each do |x|
+  x.destroy
+end
 User.create(email:'marcoaacoliveira@gmail.com', password:'123456', password_confirmation: '123456', name:'Marco Oliveira', admin: true)
 User.create(email:'coach@gmail.com', password:'123456', password_confirmation: '123456', name:'Coach', admin: false)
 
+
+u = Team.all
+u.each do |x|
+  x.destroy
+end
 # importando .csv
 require 'csv'
 
