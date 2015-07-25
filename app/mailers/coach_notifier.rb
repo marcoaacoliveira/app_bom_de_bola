e631648451119104468a6821bc8eebc8e787405d
Marco <marcooliveira@infojr.com.br>
2015-07-25 08:36:31 -0300

    Adicionado mailer para coaches

---
+++ app/mailers/coach_notifier.rb
@@ -0,0 +1,3 @@
+class CoachNotifier < ActionMailer::Base
+  default from: "from@example.com"
+end
---
+++ test/mailers/previews/coach_notifier_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/coach_notifier
+class CoachNotifierPreview < ActionMailer::Preview
+
+end
