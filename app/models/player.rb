2e5782f9c67f0b808180967c16a9184e0dec8add
Marco <marcooliveira@infojr.com.br>
2015-07-25 07:37:05 -0300

    Adicionar cartões a jogador atribui cartões ao time ao qual o jogador faz parte.

--- app/models/player.rb
+++ app/models/player.rb
@@ -9,19 +9,24 @@ class Player < ActiveRecord::Base
         self.yellow = 0
         self.punishment += 1
         self.total_yellow += 1
+        self.team.yellow += 1
         if(red==1)
           self.punishment += 1
           self.red += 1
+          self.team.red += 1
         end
       end
     else
         if(yellow==2)
+          self.team.red += 1
           self.red+=1
           yellow -= 2
         end
         self.yellow += yellow
         self.total_yellow += yellow
+        self.team.yellow += yellow
         self.red+= red
+        self.team.red += red
     end
   end
 
