b1d6e7b57b20eb04812dcb3130b74589e191b62e
Marco <marcooliveira@infojr.com.br>
2015-07-25 11:19:12 -0300

    Tentando resolver problemas e bugs

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,6 @@
 class Match < ActiveRecord::Base
-  belongs_to :team, :class_name => 'User'
-  belongs_to :challenger, :class_name => 'User'
-  belongs_to :where
+  belongs_to :team_id
+  belongs_to :challenger_id
+  belongs_to :where_id
   belongs_to :field
 end
